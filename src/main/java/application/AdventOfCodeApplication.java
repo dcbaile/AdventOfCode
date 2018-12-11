eb74d203f7baeffff192a393b32942eb8cf9ba1a
Dylan Bailey <dylancbailey@outlook.com>
2018-12-10 21:08:24 -0500

    Used a smaller sample to debug the real issue

--- src/main/java/application/AdventOfCodeApplication.java
+++ src/main/java/application/AdventOfCodeApplication.java
@@ -4,7 +4,8 @@ import java.io.*;
 
 public class AdventOfCodeApplication {
 	public static void main(String[] args)throws IOException {
-		File file = new File("/Users/dylanbailey/IdeaProjects/AdventOfCode/src/main/resources/adventFrequencyNumbers.txt");
+		File file = new File("C:\\Users\\dcbaile\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\adventFrequencyNumbers.txt");
+		//File newFile = new File("C:\\Users\\dcbaile\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\sample.txt");
 		String frequency = FrequencyCalibrator.getFrequency(file);
 		System.out.println(frequency);
 	}
--- src/main/java/application/FrequencyCalibrator.java
+++ src/main/java/application/FrequencyCalibrator.java
@@ -1,22 +1,39 @@
 package application;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FrequencyCalibrator {
 	private FrequencyCalibrator(){
 
 	}
-	public static String getFrequency(File calibrationFile)throws IOException{
-		try(BufferedReader br = new BufferedReader(new FileReader(calibrationFile))){
-			String str ;
-			int frequency = 0;
-			int oldFrequency = 0;
-			while((str = br.readLine())!= null){
-				oldFrequency = frequency;
-				frequency+= Integer.valueOf(str);
-				System.out.format("Current frequency is %d, change of %d; result frequency %d \n", oldFrequency, Integer.valueOf(str), frequency);
+	public static String getFrequency(File calibrationFile)throws IOException {
+		String str;
+		Integer frequency = 0;
+		int oldFrequency = 0;
+		int count = 0;
+		boolean repeated = false;
+		Map<String, Integer> hm = new HashMap();
+		hm.put(String.valueOf(0), 1);
+		while (repeated == false) {
+			try (BufferedReader br = new BufferedReader(new FileReader(calibrationFile))) {
+				while ((str = br.readLine()) != null) {
+					oldFrequency = frequency;
+					frequency += Integer.valueOf(str);
+					if (hm.containsKey(String.valueOf(frequency))) {
+						repeated = true;
+						System.out.println("The first repeating frequency is : " + frequency);
+					} else {
+						hm.put(String.valueOf(frequency), 1);
+					}
+					System.out.format("Current frequency is %d, change of %d; result frequency %d \n", oldFrequency, Integer.valueOf(str), frequency);
+				}
+				//System.out.println(hm.size());
+				//System.out.println("iteration finished");
+				//System.out.println("*******************************************\n\n\n\n\n");
 			}
-			return String.valueOf(frequency);
 		}
+		return String.valueOf(frequency);
 	}
 }
